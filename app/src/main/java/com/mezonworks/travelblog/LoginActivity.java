a4c9d8d42acec8f723dffd225d2c9bfd1dbe1915
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2022-02-15 20:11:10 +0100

    Initial commit

---
+++ app/src/main/java/com/mezonworks/travelblog/LoginActivity.java
@@ -0,0 +1,86 @@
+package com.mezonworks.travelblog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout textUsernameInput;
+    private TextInputLayout textPasswordInput;
+    private Button loginButton;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Bind the views from XML
+        textUsernameInput = findViewById(R.id.textUsernameInput);
+        textPasswordInput = findViewById(R.id.textPasswordInput);
+        loginButton = findViewById(R.id.loginButton);
+        progressBar = findViewById(R.id.progressBar);
+
+        loginButton.setOnClickListener(view -> LoginActivity.this.onLoginClicked());
+
+        textUsernameInput.getEditText().addTextChangedListener(createTextWatcher(textUsernameInput));
+        textPasswordInput.getEditText().addTextChangedListener(createTextWatcher(textPasswordInput));
+    }
+
+    private TextWatcher createTextWatcher(TextInputLayout textInputLayout) {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // not needed
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                textInputLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // not needed
+            }
+        };
+    }
+
+    private void onLoginClicked() {
+        String username = textUsernameInput.getEditText().getText().toString();
+        String password = textPasswordInput.getEditText().getText().toString();
+        if (username.isEmpty()) {
+            textUsernameInput.setError("Username must not be empty");
+        } else if (password.isEmpty()) {
+            textPasswordInput.setError("Password must not be empty");
+        } else if (!username.equals("admin") || !password.equals("admin")) {
+            showErrDialog();
+        } else {
+            performLogin();
+        }
+    }
+
+    private void performLogin() {
+        textUsernameInput.setEnabled(false);
+        textPasswordInput.setEnabled(false);
+        loginButton.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Login Failed")
+                .setMessage("Username or password is not correct. Please try again.")
+                .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
+                .show();
+    }
+}
