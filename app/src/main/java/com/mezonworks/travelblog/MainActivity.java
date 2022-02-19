f1793126430d919066dd7828166bb82df029e454
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2022-02-19 22:07:27 +0100

    Details Screen - Building Layout
    - Displaying Data From Code

--- app/src/main/java/com/mezonworks/travelblog/MainActivity.java
+++ app/src/main/java/com/mezonworks/travelblog/MainActivity.java
@@ -2,7 +2,9 @@ package com.mezonworks.travelblog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        TextView mainTextView = findViewById(R.id.mainTextView);
+//        mainTextView.setText("Hello sudoazek");
+
+        startActivity(new Intent(this, BlogDetailsActivity.class));
+        finish();
     }
 }
